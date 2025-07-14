2d0118c4d06848490ad06ed109835a0d941944fc
Shika Rai <raishika18@gmail.com>
2025-07-14 22:15:03 +0530

    Add second largest number program

---
+++ second largest.py
@@ -0,0 +1,11 @@
+a=int(input("Enter num 1:"))
+b=int(input("Enter num 2:"))
+c=int(input("Enter num 3: "))
+if(a>=b and a<=c):
+    print("the second largest is:", a)
+elif(b>=a and b<=c):
+    print("the second largest is:",b)
+else:
+    print("The second largest is:",c)    
+
+
