ef334000caeb46bc19c26d67ce03038d2fdb5510
Shika Rai <raishika18@gmail.com>
2025-07-17 21:08:27 +0530

    Add pattern printing

---
+++ patternprinting.py
@@ -0,0 +1,20 @@
+rows=int(input("Enter the number of rows"))
+num=1
+for i in range(1,rows+1):
+  if(i%2==1):
+    for j in range(1,i+1):
+      print(num,end=" ")
+      if(j<i):
+        print("*",end=" ")
+      num=num+1
+  else:
+     temp=num+i-1
+     for j in range(1,i+1):
+      print(temp,end=" ")
+      if(j<i):
+        print("*",end=" ")
+      temp=temp-1
+      num=num+1
+  print()
+
+
